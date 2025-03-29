ac92f978c7be0e4221c29577ed1a9e90e1086a28
Nishita-codes <nishitapatidar03@gmail.com>
2025-03-30 02:04:13 +0530

    added files

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,22 @@
 export const About = () => {
-    return <h1>About Page</h1>
+    return (
+    <section className="section-about container">
+      <h2 className="container-title">
+        Here are the Interesting Facts 
+        <br />
+        we're proud of
+      </h2>
+
+      <div className="gradient-cards">
+        <div className="card">
+            <div className="container bg-blue-box">
+                <p className="card-title">India</p>
+                <p>
+                    <span className=""></span>
+                </p>
+            </div>
+        </div>
+      </div>
+    </section>
+    );
 }
