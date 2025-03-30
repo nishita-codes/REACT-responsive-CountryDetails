021da6d2f9c537f11df08010581c9eafdb3d7210
Nishita-codes <nishitapatidar03@gmail.com>
2025-03-31 02:52:48 +0530

    added files

--- src/components/UI/Footers.jsx
+++ src/components/UI/Footers.jsx
@@ -1,17 +1,3 @@
 export const Footers = () => {
-    return (
-        <section className="section-contact">
-            <h2 className="container-title">Contact Us</h2>
-
-        <div className="contact-wrapper container">
-            <form action="">
-               <input type="text" className="form-control" required autoComplete="false" placeholder="Enter your name" name="username" />
-               <input type="email" className="form-control" required autoComplete="false" placeholder="Enter your email"  />
-               <textarea className="form-control" rows="10" placeholder="Enter your message" name="message" required autoComplete="false"></textarea>
-
-               <button className="send" type="submit" value="send">send</button>
-            </form>
-            </div>
-        </section>
-    )
+    return <h1>Hello Footer</h1>
 };
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,17 @@
 export const Contact = () => {
-    return <h1>Contact Page</h1>
+    return (
+        <section className="section-contact">
+            <h2 className="container-title1">Contact Us</h2>
+
+        <div className="contact-wrapper container">
+            <form action="">
+               <input type="text" className="form-control" required autoComplete="false" placeholder="Enter your name" name="username" />
+               <input type="email" className="form-control" required autoComplete="false" placeholder="Enter your email"  />
+               <textarea className="form-control" rows="10" placeholder="Enter your message" name="message" required autoComplete="false"></textarea>
+
+               <button className="send" type="submit" value="send">send</button>
+            </form>
+            </div>
+        </section>
+    )
 }
