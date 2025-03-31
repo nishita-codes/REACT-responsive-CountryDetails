a6eb616689a23e1a99142b52793a2986dd718cce
Nishita-codes <nishitapatidar03@gmail.com>
2025-04-01 00:25:29 +0530

    added files

--- src/components/UI/Footers.jsx
+++ src/components/UI/Footers.jsx
@@ -1,8 +1,16 @@
 import footerContact from "../../api/footerApi.json";
-
+import { MdPlace } from "react-icons/md";
+import { IoCallSharp } from "react-icons/io5";
+import { TbMailPlus } from "react-icons/tb";
 
 export const Footers = () => {
-    return (<footer className="footer-section">
+    const footerIcon = {
+        MdPlace: <MdPlace/>,
+       IoCallSharp : <IoCallSharp />,
+       TbMailPlus : <TbMailPlus />,
+     }
+    return (
+    <footer className="footer-section">
        <div className="container grid grid-three-cols">
         {
           footerContact.map((curData , index) =>{
@@ -10,7 +18,7 @@ export const Footers = () => {
 
            return (
             <div className="footer-contact" key={index}>
-                <div className="icon">{icon}</div>
+                <div className="icon">{footerIcon[icon]}</div>
                 <div className="footer-contact-text">
                     <p>{title}</p>
                     <p>{details}</p>
@@ -20,6 +28,17 @@ export const Footers = () => {
           })
         }
        </div>
+       
+       <div className="copyright-area">
+         <div className="container">
+           
+             <div className="copyright-text">
+               <p>
+                 Copyright &copy; 2024, All Right Reserved.
+               </p>
+             </div>
+            </div>
+         </div>
     </footer>
     );
 };
