a6d2f975ba7d54261ac8c428dc3630f2c5600b5b
Nishita-codes <nishitapatidar03@gmail.com>
2025-03-31 23:40:56 +0530

    added files

--- src/components/UI/Footers.jsx
+++ src/components/UI/Footers.jsx
@@ -1,3 +1,25 @@
+import footerContact from "../../api/footerApi.json";
+
+
 export const Footers = () => {
-    return <h1>Hello Footer</h1>
+    return (<footer className="footer-section">
+       <div className="container grid grid-three-cols">
+        {
+          footerContact.map((curData , index) =>{
+           const {icon,title,details} = curData;
+
+           return (
+            <div className="footer-contact" key={index}>
+                <div className="icon">{icon}</div>
+                <div className="footer-contact-text">
+                    <p>{title}</p>
+                    <p>{details}</p>
+                </div>
+            </div>
+           );
+          })
+        }
+       </div>
+    </footer>
+    );
 };
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,15 @@
 export const Contact = () => {
+
+    const handleFormSubmit = (formData)=>{
+        const formInputData = Object.fromEntries(formData.entries()); // data laane ke liye
+        console.log(formInputData);
+    }
     return (
         <section className="section-contact">
             <h2 className="container-title1">Contact Us</h2>
 
         <div className="contact-wrapper container">
-            <form action="">
+            <form action={handleFormSubmit}>
                <input type="text" className="form-control" required autoComplete="false" placeholder="Enter your name" name="username" />
                <input type="email" className="form-control" required autoComplete="false" placeholder="Enter your email"  />
                <textarea className="form-control" rows="10" placeholder="Enter your message" name="message" required autoComplete="false"></textarea>
