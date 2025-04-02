56746e7857d824369408c150984283844d5e446b
Nishita-codes <nishitapatidar03@gmail.com>
2025-04-02 22:45:41 +0530

    added files

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,3 +1,12 @@
+import { useEffect, useTransition } from "react";
+
 export const Country = () => {
+    const [isPending, startTransition] = useTransition();
+     
+    useEffect(() => {
+      
+    },[]);
+
+    if(isPending) return <h1>Loading...</h1>;
     return <h1>Country Page</h1>;
 }
